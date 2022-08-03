f821d34eaa3d67f0a03d98a48f0a4e6f43ac54f1
bhumika1111 <bhoyarpatil@434>
2022-08-03 10:26:18 +0530

    Check Employee Wage After Adding Part Time

---
+++ EmpWagePartTime.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageComputation
+{
+    /// <summary>
+    /// Here We Check Only Employee Is Present Or Absent By Using Random Function.
+    /// Note:Here We Write Code Using Non-Static Method.
+    /// </summary>
+    public class EmpWagePartTime
+    {
+        /// <summary>
+        /// Checks the employee part time.
+        /// </summary>
+        public void CheckEmployeePartTime()
+        {
+            //constant
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+
+            //variables
+
+            int empwage = 0;
+            int empHrs = 0;
+
+            Random random = new Random();
+
+            int empCheck = random.Next(3);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee Is Full Time");
+                empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee Is Part Time");
+                empHrs = 4;
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+                empHrs = 0;
+            }
+            empwage = EMP_RATE_PER_HOUR * empHrs;
+            Console.WriteLine("Employee Wage:" + empwage);
+
+        }
+    }
+}
