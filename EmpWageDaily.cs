4f8b26a6667233fbcbfb63a92767eb29438c888a
bhumika1111 <bhoyarpatil@434>
2022-08-03 10:22:25 +0530

    Check Employee Attendance

---
+++ EmpWageDaily.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageComputation
+{
+    /// <summary>
+    /// Check The Employee Is Working For Full Time ,Then They Will be Geeting Complete Sallary For That Day.
+    /// Note : Here We Write Code Using Non-Static Method . 
+    /// <summary>
+    public class EmpWageDaily
+    {
+        /// <summary>
+        /// Checks the employee attendance.
+        /// </summary>
+        public void CheckEmpAttendanceFullTime()
+        {
+            //Constant
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            //Variables
+
+            int empHrs = 0;
+            int empWage = 0;
+
+
+            Random random = new Random();
+
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee Is Present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
+
+        }
+    }
+}
